22c3d6b9e36d078833551ef2b639217883f3d625
GeniePrinie <geniexpn@gmail.com>
2023-03-07 12:39:22 +0100

    Added multiple filterings

--- js/handlers/homeHandler/filterPosts.mjs
+++ js/handlers/homeHandler/filterPosts.mjs
@@ -1,3 +1,4 @@
+import { isValidUrl } from "../../shared/validURL.mjs";
 import { renderPosts } from "./renderPosts.mjs";
 
 const todaysPosts = document.getElementById("todays-posts");
@@ -6,13 +7,13 @@ const mediaPosts = document.getElementById("media-posts");
 const tagsPosts = document.getElementById("tags-posts");
 
 export function filterPosts(entries) {
-  createEventListener(todaysPosts, entries);
-  createEventListener(weekPosts, entries);
-  createEventListener(mediaPosts, entries);
-  createEventListener(tagsPosts, entries);
+  filterTodaysPosts(todaysPosts, entries);
+  filterWeeksPosts(weekPosts, entries);
+  filterMediaPosts(mediaPosts, entries);
+  filterTagsPosts(tagsPosts, entries);
 }
 
-function createEventListener(checkbox, entries) {
+function filterTodaysPosts(checkbox, entries) {
   checkbox.addEventListener("click", () => {
     if (checkbox.checked == true) {
       const filteredEntries = entries.filter((entry) => {
@@ -27,7 +28,61 @@ function createEventListener(checkbox, entries) {
   });
 }
 
+function filterWeeksPosts(checkbox, entries) {
+  checkbox.addEventListener("click", () => {
+    if (checkbox.checked == true) {
+      const filteredEntries = entries.filter((entry) => {
+        if (thisWeek(new Date(entry.created))) {
+          return true;
+        }
+      });
+      renderPosts(filteredEntries);
+    } else {
+      renderPosts(entries);
+    }
+  });
+}
+
+function filterMediaPosts(checkbox, entries) {
+  checkbox.addEventListener("click", () => {
+    if (checkbox.checked == true) {
+      const filteredEntries = entries.filter((entry) => {
+        if (isValidUrl(entry.media)) {
+          return true;
+        }
+      });
+      renderPosts(filteredEntries);
+    } else {
+      renderPosts(entries);
+    }
+  });
+}
+
+function filterTagsPosts(checkbox, entries) {
+  checkbox.addEventListener("click", () => {
+    if (checkbox.checked == true) {
+      const filteredEntries = entries.filter((entry) => {
+        if (entry.tags.length != 0) {
+          return true;
+        }
+      });
+      renderPosts(filteredEntries);
+    } else {
+      renderPosts(entries);
+    }
+  });
+}
+
 function isToday(someDate) {
+  const today = new Date();
+  return (
+    someDate.getDate() == today.getDate() &&
+    someDate.getMonth() == today.getMonth() &&
+    someDate.getFullYear() == today.getFullYear()
+  );
+}
+
+function thisWeek(someDate) {
   const today = new Date();
   return (
     someDate.getDate() == today.getDate() &&
